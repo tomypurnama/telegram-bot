f567a8eaf67a4134b8896f44cc420100d1306c3c
tomypurnama <tomypurnamaputra@gmail.com>
2026-03-21 14:34:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request
+from flask_cors import CORS
 import dns.resolver
 
 app = Flask(__name__)
 
+# WAJIB: Biar bisa diakses dari GitHub Pages
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/")
+def home():
+    return "API RUNNING"
+
 def cek(domain):
     resolver = dns.resolver.Resolver()
 
+    # DNS Nawala
     resolver.nameservers = ["180.131.144.144"]
     try:
         resolver.resolve(domain, "A")
@@ -13,6 +22,7 @@ def cek(domain):
     except:
         pass
 
+    # Fallback DNS global
     resolver.nameservers = ["8.8.8.8"]
     try:
         resolver.resolve(domain, "A")
